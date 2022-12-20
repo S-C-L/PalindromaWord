f0778f084ca51b47ba533ca519f50c9ca9322af1
ALIEN55 <ALIEN55@LAPTOP-QQ07NK59>
2022-12-20 13:24:45 -0600

    Add project files.

---
+++ PalindromaWord/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string palabra, inverso, caracter;
+        int i;
+        Console.WriteLine("Ingrese palabra");
+         palabra = Convert.ToString(Console.ReadLine());
+       // palabra = "anitalavalatina";
+        i = palabra.Length;
+        inverso = "";
+        for (int x = i-1; x >= 0; x--)
+        {
+            caracter = palabra.Substring(x,1);
+            inverso = inverso + caracter;
+        }
+        if(palabra == inverso)
+        {
+            Console.WriteLine("Es palindroma");
+        }
+        else
+        {
+            Console.WriteLine("No es palindroma");
+        }
+        Console.ReadKey();
+
+
+    }
+}
